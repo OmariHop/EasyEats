fae70ff6e4eab3ed97c2d6a2bfc24a7e2a589e05
OmariHop <ohopkins41@gmail.com>
2024-03-06 05:17:32 -0500

    First additional changes to project, added initial homepage and removed pre exisiting styling for the following pages

--- dui9g3_1/src/App.js
+++ dui9g3_1/src/App.js
@@ -1,18 +1,25 @@
 import "./styles.css";
 import "./App.css";
+import "./HomePage.css";
+
+import HomePage from "./Homepage";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import { useState } from "react";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const [firstStepOption, setFirstStepOption] = useState("initial");
   const [secondStepOption, setSecondStepOption] = useState("second");
 
   return (
-    <div className="backgroundImg">
-      <div className="whiteRectangle">
+    <div className="App">
+        {currentPage === 0 && (
+          <HomePage
+            nextPage={() => setCurrentPage(1)}
+            />
+        )}        
         {currentPage === 1 && (
           <Step1
             setValue={setFirstStepOption}
@@ -32,7 +39,6 @@ export default function App() {
             nextPage={() => setCurrentPage(1)}
           />
         )}
-      </div>
-    </div>
+        </div>
   );
 }
---
+++ dui9g3_1/src/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function HomePage({nextPage}) {
+    return ( 
+        <div>
+            <h1>Welcome to EasyEats</h1>
+            <h2>Click the button below to get started and enduglge in future dining!</h2>
+        </div>
+
+    );
+
+
+
+}
